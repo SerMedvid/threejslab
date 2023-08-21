4a1f0629352f02c564a58f2865a82cda541b5020
Sergii Medvid <mr.medvid@gmail.com>
2023-08-21 17:18:57 +0300

    added shadows

--- features/ShoeConfigurator/components/Experience.tsx
+++ features/ShoeConfigurator/components/Experience.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Float, Html, OrbitControls } from "@react-three/drei";
+import { ContactShadows, Float, Html, OrbitControls } from "@react-three/drei";
 import { ShoeModel } from "./ShoeModel";
 import ClickIndicator from "./ClickIndicator";
 import { Suspense } from "react";
@@ -13,11 +13,26 @@ export default function Experience() {
 			<Float>
 				<Suspense fallback={null}>
 					<ShoeModel />
+					<ContactShadows
+						position={[0, -0.8, 0]}
+						opacity={0.25}
+						scale={10}
+						blur={1.5}
+						far={0.8}
+					/>
+
 					<Html>
 						<ClickIndicator />
 					</Html>
 				</Suspense>
 			</Float>
+			<ContactShadows
+				position={[0, -0.8, 0]}
+				opacity={0.25}
+				scale={10}
+				blur={1.5}
+				far={0.8}
+			/>
 
 			<OrbitControls
 				enableZoom={false}
