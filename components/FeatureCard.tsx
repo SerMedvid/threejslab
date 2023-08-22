0d5ce8e06e966b105880ca7bd7776a70e8f3c5c1
Sergii Medvid <mr.medvid@gmail.com>
2023-08-22 10:19:52 +0300

    styles update

--- components/FeatureCard.tsx
+++ components/FeatureCard.tsx
@@ -10,7 +10,7 @@ export default function FeatureCard({
 	return (
 		<Link
 			href={link}
-			className="border block border-solid border-blue-950 p-4 rounded-lg after:content-[''] relative overflow-hidden after:w-full after:h-full after:absolute after:bg-blue-950 after:top-0 after:left-0 after:opacity-0 hover:after:opacity-100 after:-z-10 transition-colors duration-300 hover:text-white after:transition-all after:duration-300 h-full"
+			className="border-4 block border-solid border-blue-950 p-4 rounded-2xl after:content-[''] relative overflow-hidden after:w-full after:h-full after:absolute after:bg-blue-950 after:top-0 after:left-0 after:opacity-0 hover:after:opacity-100 after:-z-10 transition-colors duration-300 hover:text-white after:transition-all after:duration-300 h-full"
 		>
 			<h3 className="font-semibold">{headline}</h3>
 
@@ -18,7 +18,7 @@ export default function FeatureCard({
 				<ul className="flex gap-2 flex-wrap mt-2 mb-4">
 					{tags.map((tag) => (
 						<li
-							className="bg-gray-400 px-2 py-1 text-white font-light text-xs rounded-full"
+							className="bg-gray-400 px-2 py-1 text-white font-semibold text-xs rounded-full"
 							key={tag}
 						>
 							{tag}
