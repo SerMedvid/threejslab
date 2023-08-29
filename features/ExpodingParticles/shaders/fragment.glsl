404aed35561e01be9b3e5901c55b3bc6c66e3e1c
Sergii Medvid <mr.medvid@gmail.com>
2023-08-29 10:56:47 +0300

    code cleanup

--- features/ExpodingParticles/shaders/fragment.glsl
+++ features/ExpodingParticles/shaders/fragment.glsl
@@ -8,11 +8,8 @@ varying vec3 vPosition;
 float PI = 3.141592653589793;
 
 void main() {
-    // vec2 newUV = vPosition.xy / vec2(480. * 1.5, 820. * 1.5) + vec2(0.5);
-    // gl_FragColor = vec4(1., 0., 0., 1.);
     vec4 ttStart = texture2D(uTStart, vUv);
     vec4 ttEnd = texture2D(uTEnd, vUv);
-    // gl_FragColor = vec4(vUv, 0., 1.);
 
     vec4 finalTexture = mix(ttStart, ttEnd, uProgress);
     gl_FragColor = finalTexture;
